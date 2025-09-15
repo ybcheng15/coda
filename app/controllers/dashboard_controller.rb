4119f2407341555cc55455c7e1b1f16219e921d6
YC <yibo1121996462@gmail.com>
2025-09-14 22:07:44 -0400

    feat: flowbite + time period tabs and  sign out button

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,8 +9,18 @@ class DashboardController < ApplicationController
       @profile = client.profile
       @playback_state = client.playback_state(@profile["country"])
       # puts "Playback state: #{@playback_state["item"]["name"]}"
-      @top_artists = client.top_artists(20, "short_term")
-      @top_tracks = client.top_tracks(20, "short_term")
+
+      @top_artists = {
+        short_term: client.top_artists(50, "short_term"),
+        medium_term: client.top_artists(50, "medium_term"),
+        long_term: client.top_artists(50, "long_term")
+      }
+
+      @top_tracks = {
+        short_term: client.top_tracks(50, "short_term"),
+        medium_term: client.top_tracks(50, "medium_term"),
+        long_term: client.top_tracks(50, "long_term")
+      }
     end
   end
 
